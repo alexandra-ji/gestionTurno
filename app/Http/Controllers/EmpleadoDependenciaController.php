506b40a0705a6ea27a9ea0e6ba91a5619ef41b4a
Jimena Camacho <camachojimena177@gmail.com>
2025-10-03 08:32:41 -0500

    se agregor los request al controlador

---
+++ app/Http/Controllers/EmpleadoDependenciaController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\EmpleadoDependencia;
+use Illuminate\Http\Request;
+
+class EmpleadoDependenciaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(EmpleadoDependencia $empleadoDependencia)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(EmpleadoDependencia $empleadoDependencia)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, EmpleadoDependencia $empleadoDependencia)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(EmpleadoDependencia $empleadoDependencia)
+    {
+        //
+    }
+}
